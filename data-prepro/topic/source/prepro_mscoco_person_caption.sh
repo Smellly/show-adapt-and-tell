6f2f3119c5a33ebb90b3afdc4cb27342404c7244
jaycshen <schenxmu@gmail.com>
2018-05-04 14:32:59 +0800

    modify topic dir

---
+++ data-prepro/topic/source/prepro_mscoco_person_caption.sh
@@ -0,0 +1,7 @@
+# python preprocess_entity.py train
+# python preprocess_entity.py test
+# python preprocess_entity.py val
+
+python preprocess_token.py train
+python preprocess_token.py val
+python preprocess_token.py test
